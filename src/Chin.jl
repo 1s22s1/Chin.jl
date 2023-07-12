e5bfb7acbffe6a1f20c7c615f84af5c61d91399c
1s22s1 <1s22s1@example.com>
2023-07-13 07:00:39 +0900

    Define arrowKey

--- src/Chin.jl
+++ src/Chin.jl
@@ -5,15 +5,19 @@ module Chin
         acceptedStatus = 1
 
         for char in split(pattern, "")
-            arrows[char] = 1
+            arrows[arrowKey(char, 0)] = 1
         end
 
-        if haskey(arrows, s)
-            currentStatus = arrows[s]
+        if haskey(arrows, arrowKey(s, 0))
+            currentStatus = arrows[arrowKey(s, 0)]
         else
             return false
         end
 
         return currentStatus == acceptedStatus
+    end
+
+    function arrowKey(input, currentStatus)
+        string(input, "-", currentStatus)
     end
 end
