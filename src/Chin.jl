9fb1071ee7d05a2c148f953d6b272467b53a3eaf
1s22s1 <1s22s1@example.com>
2023-07-15 22:13:20 +0900

    Define matchOne

--- src/Chin.jl
+++ src/Chin.jl
@@ -1,71 +1,13 @@
 module Chin
-    function regexp(string, pattern, actionPattern="connection")
-        if actionPattern == "connection"
-            return connectionRegexp(string, pattern)
-        else
-            return selectRegexp(string, pattern)
+    function matchOne(pattern, text)
+        if text == ""
+            return false
         end
-    end
 
-    function connectionRegexp(string, pattern)
-        if length(string) == 0
+        if pattern == "."
             return true
         end
 
-        currentStatus = 0
-        arrows = makeConnectionArrows(pattern)
-        acceptedStatus = length(pattern)
-
-        for char ∈ split(string, "")
-            currentArrowkey = arrowKey(currentStatus, char)
-            
-            if currentArrowkey ∈ keys(arrows)
-                currentStatus = arrows[currentArrowkey]
-            else
-                break
-            end
-        end
-
-        return currentStatus == acceptedStatus
+        return pattern == text
     end
-
-    function makeConnectionArrows(pattern)
-        nodeNumber = 0
-        arrows = Dict()
-
-        for char ∈ split(pattern, "")
-            arrows[arrowKey(nodeNumber, char)] = nodeNumber + 1
-            nodeNumber += 1
-        end
-
-        return arrows
-    end
-
-    function selectRegexp(string, pattern)
-        if length(string) == 0
-            return true
-        end
-
-        currentStatus = 0
-        arrows = makeSelectArrows(pattern)
-        acceptedStatus = 1
-
-        if arrowKey(0, string) ∈ keys(arrows)
-            currentStatus = 1
-        end
-
-        return currentStatus == acceptedStatus
-    end
-
-    function makeSelectArrows(pattern)
-        arrows = Dict()
-
-        for char ∈ split(pattern, '|')
-            arrows[arrowKey(0, char)] = 1
-        end
-
-        return arrows
-    end
-
-    arrowKey(currentStatus, input) = string(currentStatus, "-", input)
 end
