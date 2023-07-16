d580d630fd089d1b67160002df89882de0b3a205
1s22s1 <1s22s1@example.com>
2023-07-16 11:19:11 +0900

    Revert "Define matchOne"
    This reverts commit 9fb1071ee7d05a2c148f953d6b272467b53a3eaf.

--- src/Chin.jl
+++ src/Chin.jl
@@ -1,13 +1,71 @@
 module Chin
-    function matchOne(pattern, text)
-        if text == ""
-            return false
+    function regexp(string, pattern, actionPattern="connection")
+        if actionPattern == "connection"
+            return connectionRegexp(string, pattern)
+        else
+            return selectRegexp(string, pattern)
         end
+    end
 
-        if pattern == "."
+    function connectionRegexp(string, pattern)
+        if length(string) == 0
             return true
         end
 
-        return pattern == text
+        currentStatus = 0
+        arrows = makeConnectionArrows(pattern)
+        acceptedStatus = length(pattern)
+
+        for char ∈ split(string, "")
+            currentArrowkey = arrowKey(currentStatus, char)
+            
+            if currentArrowkey ∈ keys(arrows)
+                currentStatus = arrows[currentArrowkey]
+            else
+                break
+            end
+        end
+
+        return currentStatus == acceptedStatus
     end
+
+    function makeConnectionArrows(pattern)
+        nodeNumber = 0
+        arrows = Dict()
+
+        for char ∈ split(pattern, "")
+            arrows[arrowKey(nodeNumber, char)] = nodeNumber + 1
+            nodeNumber += 1
+        end
+
+        return arrows
+    end
+
+    function selectRegexp(string, pattern)
+        if length(string) == 0
+            return true
+        end
+
+        currentStatus = 0
+        arrows = makeSelectArrows(pattern)
+        acceptedStatus = 1
+
+        if arrowKey(0, string) ∈ keys(arrows)
+            currentStatus = 1
+        end
+
+        return currentStatus == acceptedStatus
+    end
+
+    function makeSelectArrows(pattern)
+        arrows = Dict()
+
+        for char ∈ split(pattern, '|')
+            arrows[arrowKey(0, char)] = 1
+        end
+
+        return arrows
+    end
+
+    arrowKey(currentStatus, input) = string(currentStatus, "-", input)
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,26 +1,62 @@
 using Test
 
 import Chin:
-matchOne
+makeConnectionArrows, connectionRegexp, makeSelectArrows, selectRegexp, regexp
 
-@testset "1文字の場合" begin
-    @testset "完全一致する場合" begin
-        @test matchOne("a", "a") == true
-        @test matchOne("b", "b") == true
+@testset "連接の場合" begin
+    @testset "遷移の作成" begin
+        @test makeConnectionArrows("a") == Dict("0-a" => 1)
+        @test makeConnectionArrows("ab") == Dict("0-a" => 1, "1-b" => 2)
+        @test makeConnectionArrows("abc") == Dict("0-a" => 1, "1-b" => 2, "2-c" => 3)
     end
 
-    @testset ".で一致する場合" begin
-        @test matchOne(".", "a") == true
-        @test matchOne(".", "b") == true
+    @testset "0文字の場合" begin
+        @test connectionRegexp("", "a") == true
+        @test connectionRegexp("", "b") == true
     end
 
-    @testset "対象文字列が空の場合" begin
-        @test matchOne("a", "") == false
-        @test matchOne(".", "") == false
+    @testset "1文字の場合" begin
+        @test connectionRegexp("a", "a") == true
+        @test connectionRegexp("b", "a") == false
     end
 
-    @testset "一致しない場合" begin
-        @test matchOne("a", "b") == false
-        @test matchOne("a", "c") == false
+    @testset "2文字の場合" begin
+        @test connectionRegexp("ab", "ab") == true
+        @test connectionRegexp("aa", "ab") == false
     end
+
+    @testset "正規表現と対象文字列の長さが異なる場合" begin
+        @test connectionRegexp("a", "ab") == false
+    end
+end
+
+@testset "選択の場合" begin
+    @testset "遷移の作成" begin
+        @test makeSelectArrows("a|b") == Dict("0-a" => 1, "0-b" => 1)
+        @test makeSelectArrows("a|b|c") == Dict("0-a" => 1, "0-b" => 1, "0-c" => 1)
+        @test makeSelectArrows("a|b|c|d") == Dict("0-a" => 1, "0-b" => 1, "0-c" => 1, "0-d" => 1)
+    end
+
+    @testset "0文字の場合" begin
+        @test connectionRegexp("", "a|b") == true
+        @test connectionRegexp("", "a|b|c") == true
+    end
+
+    @testset "2つからの選択の場合" begin
+        @test selectRegexp("a", "a|b") == true
+        @test selectRegexp("b", "a|b") == true
+        @test selectRegexp("c", "a|b") == false
+    end
+
+    @testset "3つからの選択の場合" begin
+        @test selectRegexp("a", "a|b|c") == true
+        @test selectRegexp("b", "a|b|c") == true
+        @test selectRegexp("c", "a|b|c") == true
+        @test selectRegexp("d", "a|b|c") == false
+    end
+end
+
+@testset "総合テスト" begin
+    @test regexp("abcd", "abcd", "connection") == true
+    @test regexp("a", "a|b|c", "select") == true
 end
