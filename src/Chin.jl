f0534a80c4f9ca080b95ad5e48deaf8b8870ba90
1s22s1 <1s22s1@example.com>
2023-07-18 05:29:07 +0900

    Define matchOneAutomaton

--- src/Chin.jl
+++ src/Chin.jl
@@ -1,71 +1,80 @@
 module Chin
-    function regexp(string, pattern, actionPattern="connection")
-        if actionPattern == "connection"
-            return connectionRegexp(string, pattern)
-        else
-            return selectRegexp(string, pattern)
-        end
+    struct Arrow
+        fromNode::Int
+        toNode::Int
+        arrowValue::String
     end
 
-    function connectionRegexp(string, pattern)
-        if length(string) == 0
-            return true
-        end
+    function matchOneAutomaton(string)
+        Arrow(0, 1, string)
+    end
+    # function regexp(string, pattern, actionPattern="connection")
+    #     if actionPattern == "connection"
+    #         return connectionRegexp(string, pattern)
+    #     else
+    #         return selectRegexp(string, pattern)
+    #     end
+    # end
 
-        currentStatus = 0
-        arrows = makeConnectionArrows(pattern)
-        acceptedStatus = length(pattern)
+    # function connectionRegexp(string, pattern)
+    #     if length(string) == 0
+    #         return true
+    #     end
 
-        for char ∈ split(string, "")
-            currentArrowkey = arrowKey(currentStatus, char)
+    #     currentStatus = 0
+    #     arrows = makeConnectionArrows(pattern)
+    #     acceptedStatus = length(pattern)
+
+    #     for char ∈ split(string, "")
+    #         currentArrowkey = arrowKey(currentStatus, char)
             
-            if currentArrowkey ∈ keys(arrows)
-                currentStatus = arrows[currentArrowkey]
-            else
-                break
-            end
-        end
+    #         if currentArrowkey ∈ keys(arrows)
+    #             currentStatus = arrows[currentArrowkey]
+    #         else
+    #             break
+    #         end
+    #     end
 
-        return currentStatus == acceptedStatus
-    end
+    #     return currentStatus == acceptedStatus
+    # end
 
-    function makeConnectionArrows(pattern)
-        nodeNumber = 0
-        arrows = Dict()
+    # function makeConnectionArrows(pattern)
+    #     nodeNumber = 0
+    #     arrows = Dict()
 
-        for char ∈ split(pattern, "")
-            arrows[arrowKey(nodeNumber, char)] = nodeNumber + 1
-            nodeNumber += 1
-        end
+    #     for char ∈ split(pattern, "")
+    #         arrows[arrowKey(nodeNumber, char)] = nodeNumber + 1
+    #         nodeNumber += 1
+    #     end
 
-        return arrows
-    end
+    #     return arrows
+    # end
 
-    function selectRegexp(string, pattern)
-        if length(string) == 0
-            return true
-        end
+    # function selectRegexp(string, pattern)
+    #     if length(string) == 0
+    #         return true
+    #     end
 
-        currentStatus = 0
-        arrows = makeSelectArrows(pattern)
-        acceptedStatus = 1
+    #     currentStatus = 0
+    #     arrows = makeSelectArrows(pattern)
+    #     acceptedStatus = 1
 
-        if arrowKey(0, string) ∈ keys(arrows)
-            currentStatus = 1
-        end
+    #     if arrowKey(0, string) ∈ keys(arrows)
+    #         currentStatus = 1
+    #     end
 
-        return currentStatus == acceptedStatus
-    end
+    #     return currentStatus == acceptedStatus
+    # end
 
-    function makeSelectArrows(pattern)
-        arrows = Dict()
+    # function makeSelectArrows(pattern)
+    #     arrows = Dict()
 
-        for char ∈ split(pattern, '|')
-            arrows[arrowKey(0, char)] = 1
-        end
+    #     for char ∈ split(pattern, '|')
+    #         arrows[arrowKey(0, char)] = 1
+    #     end
 
-        return arrows
-    end
+    #     return arrows
+    # end
 
-    arrowKey(currentStatus, input) = string(currentStatus, "-", input)
+    # arrowKey(currentStatus, input) = string(currentStatus, "-", input)
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,62 +1,8 @@
 using Test
 
-import Chin:
-makeConnectionArrows, connectionRegexp, makeSelectArrows, selectRegexp, regexp
+import Chin: Arrow, matchOneAutomaton
 
-@testset "連接の場合" begin
-    @testset "遷移の作成" begin
-        @test makeConnectionArrows("a") == Dict("0-a" => 1)
-        @test makeConnectionArrows("ab") == Dict("0-a" => 1, "1-b" => 2)
-        @test makeConnectionArrows("abc") == Dict("0-a" => 1, "1-b" => 2, "2-c" => 3)
-    end
-
-    @testset "0文字の場合" begin
-        @test connectionRegexp("", "a") == true
-        @test connectionRegexp("", "b") == true
-    end
-
-    @testset "1文字の場合" begin
-        @test connectionRegexp("a", "a") == true
-        @test connectionRegexp("b", "a") == false
-    end
-
-    @testset "2文字の場合" begin
-        @test connectionRegexp("ab", "ab") == true
-        @test connectionRegexp("aa", "ab") == false
-    end
-
-    @testset "正規表現と対象文字列の長さが異なる場合" begin
-        @test connectionRegexp("a", "ab") == false
-    end
-end
-
-@testset "選択の場合" begin
-    @testset "遷移の作成" begin
-        @test makeSelectArrows("a|b") == Dict("0-a" => 1, "0-b" => 1)
-        @test makeSelectArrows("a|b|c") == Dict("0-a" => 1, "0-b" => 1, "0-c" => 1)
-        @test makeSelectArrows("a|b|c|d") == Dict("0-a" => 1, "0-b" => 1, "0-c" => 1, "0-d" => 1)
-    end
-
-    @testset "0文字の場合" begin
-        @test connectionRegexp("", "a|b") == true
-        @test connectionRegexp("", "a|b|c") == true
-    end
-
-    @testset "2つからの選択の場合" begin
-        @test selectRegexp("a", "a|b") == true
-        @test selectRegexp("b", "a|b") == true
-        @test selectRegexp("c", "a|b") == false
-    end
-
-    @testset "3つからの選択の場合" begin
-        @test selectRegexp("a", "a|b|c") == true
-        @test selectRegexp("b", "a|b|c") == true
-        @test selectRegexp("c", "a|b|c") == true
-        @test selectRegexp("d", "a|b|c") == false
-    end
-end
-
-@testset "総合テスト" begin
-    @test regexp("abcd", "abcd", "connection") == true
-    @test regexp("a", "a|b|c", "select") == true
+@testset "一文字に対するオートマトンを作成する" begin
+    @test matchOneAutomaton("a") == Arrow(0, 1, "a")
+    @test matchOneAutomaton("ϵ") == Arrow(0, 1, "ϵ")
 end
